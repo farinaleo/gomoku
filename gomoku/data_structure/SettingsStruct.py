7dc08e749ce94ad0e338d22c8f4a33fa94e9db02
nskiba <nskiba@student.42angouleme.fr>
2024-02-23 17:44:44 +0100

    :sparkles: add SettingsStruct

---
+++ gomoku/data_structure/SettingsStruct.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class SettingsStruct:
+    """
+    SettingsStruct class
+    This class is used to store the settings for the application.
+    """
+    __fps: int = 30
+    __music: bool = True
+    __sound: bool = True
+    __fullscreen: bool = False
+    __windows_size: [int, int] = field(default=(1280, 720))
+
+    def get_fps(self) -> int:
+        return self.__fps
+
+    def set_fps(self, fps: int):
+        self.__fps = fps
+
+    def get_music(self) -> bool:
+        return self.__music
+
+    def set_music(self, music: bool):
+        self.__music = music
+
+    def get_sound(self) -> bool:
+        return self.__sound
+
+    def set_sound(self, sound: bool):
+        self.__sound = sound
+
+    def get_fullscreen(self) -> bool:
+        return self.__fullscreen
+
+    def set_fullscreen(self, fullscreen: bool):
+        self.__fullscreen = fullscreen
+
+    def get_windows_size(self) -> [int, int]:
+        return self.__windows_size
+
+    def set_windows_size(self, width: int, height: int):
+        self.__windows_size = [width, height]
+
