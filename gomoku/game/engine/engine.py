efe3c6b082a5580182f6bfaefc95ad39bee584ca
nskiba <nskiba@student.42angouleme.fr>
2024-02-24 18:59:53 +0100

    :sparkles: add init icon and function feature

--- gomoku/game/engine/engine.py
+++ gomoku/game/engine/engine.py
@@ -1,6 +1,16 @@
 from dataclasses import dataclass, field
 from gomoku import SettingsStruct
 import pygame
+import os
+
+
+def init_icon():
+    try:
+        image_path = os.path.join('gomoku', 'assets', 'img', 'logo_favicon.png')
+        favicon = pygame.image.load(image_path)
+        pygame.display.set_icon(favicon)
+    except Exception as e:
+        print(f'load favicon error: {e}')
 
 
 @dataclass
@@ -13,11 +23,14 @@ class Engine:
         self.clock = pygame.time.Clock()
         self.running = True
         self.current_screen = 'main_menu'
+        init_icon()
 
     def load_settings(self):
         self.settings = SettingsStruct()
         self.settings.load()
         self.update_settings()
+        if not self.settings.get_music():
+            pygame.mixer.music.set_volume(0)
         self.settings.print() # Debug only
 
     def update_settings(self):
@@ -32,4 +45,12 @@ class Engine:
     def get_current_screen(self):
         return self.current_screen
 
+    def get_ticks(self):
+        return pygame.time.get_ticks()# a verifi
+
+    def mute(self):
+        self.settings.set_music(not self.settings.get_music())
+
+    def get_music(self):
+        return self.settings.get_music()
 
