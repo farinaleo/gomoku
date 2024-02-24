c15435299105c594fb8225eda4839a81d95b3f0a
nskiba <nskiba@student.42angouleme.fr>
2024-02-24 11:37:23 +0100

    :bulb: update comment to sound_control.py

--- gomoku/game/engine/sound_control.py
+++ gomoku/game/engine/sound_control.py
@@ -3,6 +3,10 @@ import pygame
 
 
 def load_music(music_file: str, repeat: int = -1):
+    """Load and play music file.
+    :param music_file: music file name.
+    :param repeat: how many times to repeat. -1 for infinite.
+    """
     path = os.path.join('gomoku', 'assets', 'music', music_file)
     pygame.mixer.music.load(path)
     pygame.mixer.music.play(repeat)
