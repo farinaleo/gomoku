2ec3dbbea279419f3cfc542232298174990680ed
nskiba <nskiba@student.42angouleme.fr>
2024-02-24 19:00:04 +0100

    :sparkles: add mute button

---
+++ gomoku/game/screen/components/mute_button.py
@@ -0,0 +1,16 @@
+import pygame
+from typing import Type
+from gomoku.game.engine import Engine
+
+
+def mute_button(engine: Engine):
+	"""Mute button"""
+	image_mute = pygame.image.load('gomoku/assets/img/volume.png')
+	image_unmute = pygame.image.load('gomoku/assets/img/volume-mute.png')
+	image_mute = pygame.transform.scale(image_mute, (50, 50))
+	image_unmute = pygame.transform.scale(image_unmute, (50, 50))
+	image_mute_rect = image_mute.get_rect()
+	image_unmute_rect = image_unmute.get_rect()
+	image_mute_rect.bottomright = (engine.settings.get_width() - 30, engine.settings.get_height() - 30)
+	image_unmute_rect.bottomright = (engine.settings.get_width() - 30, engine.settings.get_height() - 30)
+	return image_mute, image_mute_rect, image_unmute, image_unmute_rect
