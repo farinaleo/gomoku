26447b65a893b0021bcef9538d008720db8d808e
nskiba <nskiba@student.42angouleme.fr>
2024-02-24 10:44:00 +0100

    :sparkles: add engine class

--- gomoku/__init__.py
+++ gomoku/__init__.py
@@ -1,2 +1,3 @@
 from .grid import Grid
 from .data_structure import SettingsStruct
+from .game import Engine
---
+++ gomoku/game/engine/engine.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass, field
+from gomoku import SettingsStruct
+import pygame
+
+
+@dataclass
+class Engine:
+
+    def __init__(self):
+        pygame.init()
+        self.settings = None
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def load_settings(self):
+        self.settings = SettingsStruct()
+        self.settings.load()
+        self.update_settings()
+        self.settings.print() # Debug only
+
+    def update_settings(self):
+        self.screen = pygame.display.set_mode(self.settings.get_window_size(), pygame.FULLSCREEN if self.settings.get_fullscreen() else 0)
+        self.clock.tick(self.settings.get_fps())
+
+    def save_settings(self):
+        self.settings.save()
+
+
