bddcf8bc0905b2c454cc10564f97cb30a2301703
nskiba <nskiba@student.42angouleme.fr>
2024-02-24 19:00:21 +0100

    :recycle: refactor code screen.py

--- gomoku/game/screen/screen.py
+++ gomoku/game/screen/screen.py
@@ -2,39 +2,42 @@ from random import choice, uniform, randint
 
 import pygame
 from gomoku.game.engine import Engine
-from gomoku.data_structure import SettingsStruct
-from gomoku.game.engine import load_music
+from gomoku.game.engine import load_music, get_image
 from gomoku.game.screen.particle import stars_effect
-
+from gomoku.game.screen.components import mute_button
 
 def main_menu(engine: Engine):
 	""" Main menu screen """
+
+	# Set title screen and music
 	pygame.display.set_caption('Gomoku - Title screen')
-	engine.screen.fill((8, 26, 43))
-	pygame.display.update()
 	load_music('title_screen.mp3')
 
-	screen_width, screen_height = engine.settings.get_window_size()
-	image = pygame.image.load('gomoku/assets/img/logo-game.png')
-	image_width = screen_width // 3
-	aspect_ratio = image.get_width() / image.get_height()
-	image_height = int(image_width / aspect_ratio)
+	# Load logo
+	logo = get_image('logo-game.png', engine.settings.get_width() // 3, engine.settings.get_height() // 3)
+	logo_rect = logo.get_rect()
+	logo_rect.center = (engine.settings.get_width() // 2, logo.get_height() // 2)
 
-	image = pygame.transform.scale(image, (image_width, image_height))
-	logo_rect = image.get_rect()
-	logo_rect.center = (screen_width // 2, image_height // 2)
+	# Mute button
+	mute = mute_button(engine)
+	print(mute)
 
+	# While loop
 	running = True
 	group_particles = pygame.sprite.Group()
 	while running:
-		dt = engine.clock.tick(engine.settings.get_fps()) / 1000
 		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+				running = False
 			if event.type == pygame.QUIT:
 				running = False
 		engine.screen.fill((8, 26, 43))
-		engine.screen.blit(image, logo_rect)
+		engine.screen.blit(logo, logo_rect)
+		is_mute = 0 if engine.settings.get_music() else 2
+		engine.screen.blit(mute[0 + is_mute], mute[1 + is_mute])
 		stars_effect(20, engine.settings.get_width(), engine.settings.get_height(), group_particles)
 		group_particles.draw(engine.screen)
-		group_particles.update(dt)
+		group_particles.update()
 		pygame.display.update()
+		engine.clock.tick(engine.settings.get_fps())
 	engine.change_screen(None)
