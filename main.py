e8386bd1d94f86c53312091246c1a6fe6d194a9e
farinaleo <leo.farina.fr@gmail.com>
2024-02-25 14:14:55 +0100

    :hammer: reset main code

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-# from gomoku import SettingsStruct
-#
-# settings = SettingsStruct()
-# settings.load()
-# settings.print()
-# settings.set_fps(20)
-# settings.save()
-import time
+from gomoku import SettingsStruct
 
-import gomoku as gmk
-
-grid = gmk.Grid(19)
-start = time.time()
-grid.add_rock(8, 0, 'q', [gmk.five_to_win])
-grid.add_rock(4, 0, 'q', [gmk.five_to_win])
-grid.add_rock(3, 1, 'q', [gmk.five_to_win])
-grid.add_rock(2, 2, 'q', [gmk.five_to_win])
-grid.add_rock(1, 3, 'q', [gmk.five_to_win])
-grid.add_rock(0, 4, 'q', [gmk.five_to_win])
-for line in grid.get_grid():
-	print(line)
+settings = SettingsStruct()
+settings.load()
+settings.print()
+settings.set_fps(20)
+settings.save()
