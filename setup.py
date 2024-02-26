b3549608fee85529cf52e0c4a3af32d166020002
farinaleo <leo.farina.fr@gmail.com>
2024-02-26 10:23:30 +0100

    :truck:rename gomoky to ft_gomoku and add the setup.py to install the package with pip (venv)

---
+++ setup.py
@@ -0,0 +1,30 @@
+#   ------------------------------------------------------------------------------------------------------------------ #
+#   contact : leo.farina.fr@gmail.com                                                                 ░▄▄▄▄░           |
+#   github : https://github.com/farinaleo                                                             ▀▀▄██►           |
+#   date : 2/26/24, 9:50 AM                                                                           ▀▀███►           |
+#                                                                                                     ░▀███►░█►        |
+#                                                                                                     ▒▄████▀▀         |
+#   ------------------------------------------------------------------------------------------------------------------ #
+#  Copyright (c) 2024.
+from setuptools import setup, find_packages
+
+setup(
+    name='ft_gomoku',
+    version='0.1',
+    author='Lfarina & Nskiba',
+    author_email='lfarina@student.42angouleme.fr & nskiba@student.42angouleme.fr',
+    description='Gomoku game AI',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
