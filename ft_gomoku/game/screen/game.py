0b02d5b4328451a6882c267c4e3b11bdd8bb52a0
nskiba <nskiba@student.42angouleme.fr>
2024-02-26 15:58:16 +0100

    :sparkles: set the board at the center of the screen

--- ft_gomoku/game/screen/game.py
+++ ft_gomoku/game/screen/game.py
@@ -28,25 +28,27 @@ def game_screen(engine: Engine):
 		events_list = []
 
 		engine.screen.fill((8, 26, 43))
-		draw_board(engine)
+		draw_board(engine, 0.01)
 		running = True
 
 		while running:
 			result = handle_events(engine, events_list)
 			if result == 'quit':
 				return
-			pygame.display.update()
+			engine.clock.tick(engine.settings.get_fps())
 
 
-def draw_board(engine: Engine):
+def draw_board(engine: Engine, sleep_time=0.0):
 	"""Draw the game board"""
 	for i in range(19):
 		for j in range(19):
+			start_x = (engine.get_window_size()[0] - 950) / 2
+			start_y = (engine.get_window_size()[1] - 950) / 2
 			color = (10, 130, 141) if (i + j) % 2 == 0 else (12, 154, 165)
 			color_bottom = (12, 88, 95) if (i + j) % 2 == 0 else (9, 110, 119)
-			square = Square(j * 50, i * 50, color, color_bottom)
+			square = Square((j * 50) + start_x, (i * 50 + start_y), color, color_bottom)
 			square.create_surface()
 			square.draw(engine.screen)
-			sleep(0.009)
-			pygame.display.update()
-
+			if sleep_time > 0:
+				pygame.display.update()
+				sleep(sleep_time)
