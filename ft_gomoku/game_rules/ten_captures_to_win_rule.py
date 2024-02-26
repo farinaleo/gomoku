1c02bfc14ded2999d5c2038071ced88519626b03
farinaleo <leo.farina.fr@gmail.com>
2024-02-26 14:00:28 +0100

    :hammer: introduce ten capture to win

--- ft_gomoku/game_rules/ten_captures_to_win_rule.py
+++ ft_gomoku/game_rules/ten_captures_to_win_rule.py
@@ -7,3 +7,18 @@
 #   ------------------------------------------------------------------------------------------------------------------ #
 #  Copyright (c) 2024.
 
+from ft_gomoku import rule, RuleStatus
+
+
+@rule()
+def ten_capture_to_win(row: int, col: int, player, grid) -> RuleStatus:
+	"""Check whether the given player captured ten stones
+	:param row: y pos
+	:param col: x pos
+	:param player: who played the move
+	:param grid: the grid
+	:return: Rule status (WIN | OK)
+	"""
+	if grid.get_captured_stones(player) >= 10:
+		return RuleStatus.WIN
+	return RuleStatus.OK
