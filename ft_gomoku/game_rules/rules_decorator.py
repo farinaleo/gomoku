a4ad21b48d88aa4d176e1b05b66d9b47034b9803
farinaleo <leo.farina.fr@gmail.com>
2024-02-29 13:49:12 +0100

    :hammer: improve  the decorator

--- ft_gomoku/game_rules/rules_decorator.py
+++ ft_gomoku/game_rules/rules_decorator.py
@@ -12,8 +12,20 @@ from ft_gomoku import RuleStatus
 
 def rule():
     def decorator(func):
-        def wrapper(row: int, col: int, player, grid, game):
+        def wrapper(row: int, col: int, player, grid, game=None):
+            """
+            Decorator for the rules, it executes actions according to the rule result.
+            :param row: the y
+            :param col: the x
+            :param player: the player to add
+            :param grid: a copy of game with the player added to check rules on it
+            :param game: the real grid game (if None any action will be taken)
+            :return: the rule status
+            """
             result = func(row, col, player, grid)
+            if game is None:
+                return result if type(result) is not tuple else result[0]
+
             if type(result) == tuple and result[0] == RuleStatus.CAPTURE:
                 for point in result[1]:
                     game.remove_rock(point[0], point[1])
