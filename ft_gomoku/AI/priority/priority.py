3ab067ffb938468aaa6b646f6c573747c2910aa4
farinaleo <leo.farina.fr@gmail.com>
2024-03-11 11:58:02 +0100

    :sparkles: create the priority evaluator to find if the AI needs to defend or attack

--- ft_gomoku/AI/priority/priority.py
+++ ft_gomoku/AI/priority/priority.py
@@ -9,16 +9,50 @@
 
 
 from ft_gomoku import Grid
+from ft_gomoku.AI import matching_cases
 
 
-def is_critical(grid: Grid, x: int, y: int, player) -> bool:
+def get_priority(grid: Grid) -> int:
 	"""
-	Checks if the move is critical (probably winning).
+	Evaluate the priority according to the IA and opponent last moves.
 	:param grid: the game.
-	:param x: coordinate x.
-	:param y: coordinate y.
-	:param player: the player who placed the stone.
-	:return: True if the move is critical, False otherwise.
+	:return: 2 if the IA has to block the opponent,
+	1 if the IA can win with one move, 0 otherwise.
 	"""
 	# is in a four alignment (or three)
-	# has captured 8 opponent stones
+	# has captured 8 opponent stones and can capture 2 more.
+	priority = max(ia_priority(grid), opponent_priority(grid))
+	print(f'priority: {priority}')
+	return priority
+
+
+def ia_priority(grid: Grid) -> int:
+	"""evaluate the IA priority according to its last move.
+	:param grid: the game.
+	:return: the priority rate.
+	"""
+	ia_last_move = grid.get_last_move()
+	if not ia_last_move:
+		return 0
+	p_4 = matching_cases(grid.line_grid, grid, ia_last_move[1],
+							ia_last_move[2], grid.player1, grid.player2,
+							grid.size, grid.size ** 2, [4], False)
+	if p_4 > 0:
+		return 1
+	return 0
+
+
+def opponent_priority(grid: Grid) -> int:
+	"""evaluate the IA priority according to its last move.
+	:param grid: the game.
+	:return: the priority rate.
+	"""
+	opponent_last_move = grid.get_last_move()
+	if not opponent_last_move:
+		return 0
+	p_4 = matching_cases(grid.line_grid, grid, opponent_last_move[1],
+							opponent_last_move[2], grid.player2, grid.player1,
+							grid.size, grid.size ** 2, [3, 4], False)
+	if p_4 > 0:
+		return 2
+	return 0
