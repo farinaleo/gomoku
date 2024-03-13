5590a9e9e512e069e8c4d997b7cc5ec4a99ac9ee
farinaleo <leo.farina.fr@gmail.com>
2024-03-13 14:40:52 +0100

    :hammer: update init

--- ft_gomoku/AI/__init__.py
+++ ft_gomoku/AI/__init__.py
@@ -11,6 +11,7 @@ from ft_gomoku.AI.heurisitic.winning_points import winning
 from ft_gomoku.AI.heurisitic.capture_stones import capture_stones
 from ft_gomoku.AI.heurisitic.matching_cases import matching_cases
 from ft_gomoku.AI.heurisitic.near_to_border import near_to_border
+from ft_gomoku.AI.heurisitic.potential_capture import potential_capture
 from ft_gomoku.AI.heurisitic.evaluate_node import heuristic
 from ft_gomoku.AI.next_generation import next_generation
 from ft_gomoku.AI.algorithm.alphabetaprunning import launch_alpha_beta
