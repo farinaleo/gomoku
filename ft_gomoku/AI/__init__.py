eca260b68e5c17301f1398c9963868f8173ae0e5
farinaleo <leo.farina.fr@gmail.com>
2024-03-13 16:40:33 +0100

    :hammer: make the ai usable with p1 and p2

--- ft_gomoku/AI/__init__.py
+++ ft_gomoku/AI/__init__.py
@@ -18,5 +18,5 @@ from ft_gomoku.AI.heurisitic.evaluate_node import heuristic
 from ft_gomoku.AI.next_generation import next_generation
 from ft_gomoku.AI.algorithm.alphabetaprunning import launch_alpha_beta
 from ft_gomoku.AI.priority.priority import get_priority
-from ft_gomoku.AI.AI import run_ia
+from ft_gomoku.AI.AI import run_ai
 
--- ft_gomoku/AI/priority/priority.py
+++ ft_gomoku/AI/priority/priority.py
@@ -12,45 +12,51 @@ from ft_gomoku import Grid
 from ft_gomoku.AI import matching_cases
 
 
-def get_priority(grid: Grid) -> int:
+def get_priority(grid: Grid, ai_value, opponent_value) -> int:
 	"""
 	Evaluate the priority according to the IA and opponent last moves.
 	:param grid: the game.
+	:param ai_value: the value associated with the AI.
+	:param opponent_value: the value associated with the opponent.
 	:return: 2 if the IA has to block the opponent,
 	1 if the IA can win with one move, 0 otherwise.
 	"""
 	# is in a four alignment (or three)
 	# has captured 8 opponent stones and can capture 2 more.
-	priority = max(ia_priority(grid), opponent_priority(grid))
+	priority = max(ia_priority(grid, ai_value, opponent_value), opponent_priority(grid, ai_value, opponent_value))
 	return priority
 
 
-def ia_priority(grid: Grid) -> int:
+def ia_priority(grid: Grid, ai_value, opponent_value) -> int:
 	"""evaluate the IA priority according to its last move.
 	:param grid: the game.
+	:param ai_value: the value associated with the AI.
+	:param opponent_value: the value associated with the opponent.
 	:return: the priority rate.
 	"""
-	ia_last_move = grid.get_last_move(grid.player1)
+	ia_last_move = grid.get_last_move(ai_value)
 	if not ia_last_move:
 		return 0
 	p_4 = matching_cases(grid.line_grid, grid, ia_last_move[1],
-							ia_last_move[2], grid.player1, grid.player2,
+							ia_last_move[2], ai_value, opponent_value,
 							grid.size, grid.size ** 2, [3, 4], False)
 	if p_4 > 0:
 		return 1
 	return 0
 
 
-def opponent_priority(grid: Grid) -> int:
+def opponent_priority(grid: Grid, ai_value, opponent_value) -> int:
 	"""evaluate the IA priority according to its last move.
 	:param grid: the game.
+	:param ai_value: the value associated with the AI.
+	:param opponent_value: the value associated with the opponent.
 	:return: the priority rate.
 	"""
-	opponent_last_move = grid.get_last_move(grid.player2)
+	opponent_last_move = grid.get_last_move(opponent_value)
 	if not opponent_last_move:
 		return 0
 	p_4 = matching_cases(grid.line_grid, grid, opponent_last_move[1],
-							opponent_last_move[2], grid.player2, grid.player1,
+							opponent_last_move[2], opponent_value, ai_value,
 							grid.size, grid.size ** 2, [3, 4], False)
 	if p_4 > 0:
 		return 2
