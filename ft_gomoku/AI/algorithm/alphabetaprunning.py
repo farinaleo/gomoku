92831557ebbb0e6d28590474fa963820aa4fa2e0
farinaleo <leo.farina.fr@gmail.com>
2024-03-16 19:02:25 +0100

    :hammer: add the depth to the heuristic weight

--- ft_gomoku/AI/algorithm/alphabetaprunning.py
+++ ft_gomoku/AI/algorithm/alphabetaprunning.py
@@ -7,68 +7,68 @@
 #   ------------------------------------------------------------------------------------------------------------------ #
 #  Copyright (c) 2024.
 
-from ft_gomoku import Grid,log
+from ft_gomoku import Grid, log
 from ft_gomoku.AI import heuristic, next_generation
 
 
 @log()
 def alpha_beta(grid: Grid, depth: int, alpha: float, beta: float, rules, ai_value, is_max=True) -> float | None:
-	"""
-	Alpha Beta pruning algorithm. Uses it to find the best move to play
-	to win the game.
-	:param grid: the game board.
-	:param depth: the depth to explore.
-	:param alpha: the alpha limit.
-	:param beta:  the beta limit.
-	:param rules: game rules
-	:param ai_value: the value used to determine the AI.
-	:param is_max: True if the turn is for the IA otherwise False.
-	:return: The node evaluation or None in case of error.
-	"""
-	if depth == 0 or grid.winning:
-		return heuristic(grid, grid.player1 if is_max else grid.player2)
-	next_gen = next_generation(grid, rules, ai_value)
-	if not next_generation:
-		return None
-	if is_max:
-		max_val = float('-inf')
-		for node in next_gen:
-			_val = alpha_beta(node, depth - 1, alpha, beta, rules, ai_value, False)
-			max_val = max(max_val, _val)
-			alpha = max(alpha, max_val)
-			if max_val >= beta:
-				return max_val
-	else:
-		max_val = float('+inf')
-		for node in next_gen:
-			_val = alpha_beta(node, depth - 1, alpha, beta, rules, ai_value)
-			max_val = min(max_val, _val)
-			beta = min(beta, max_val)
-			if max_val <= alpha:
-				return max_val
-	return max_val
+    """
+    Alpha Beta pruning algorithm (fail-soft). Uses it to find the best move to play
+    to win the game.
+    :param grid: the game board.
+    :param depth: the depth to explore.
+    :param alpha: the alpha limit.
+    :param beta:  the beta limit.
+    :param rules: game rules
+    :param ai_value: the value used to determine the AI.
+    :param is_max: True if the turn is for the IA otherwise False.
+    :return: The node evaluation or None in case of error.
+    """
+    if depth == 0 or grid.winning:
+        return heuristic(grid, grid.player1 if is_max else grid.player2) + depth
+    next_gen = next_generation(grid, rules, ai_value)
+    if not next_generation:
+        return None
+    if is_max:
+        max_val = float('-inf')
+        for node in next_gen:
+            _val = alpha_beta(node, depth - 1, alpha, beta, rules, ai_value, False)
+            max_val = max(max_val, _val)
+            alpha = max(alpha, max_val)
+            if max_val >= beta:
+                return max_val
+    else:
+        max_val = float('+inf')
+        for node in next_gen:
+            _val = alpha_beta(node, depth - 1, alpha, beta, rules, ai_value)
+            max_val = min(max_val, _val)
+            beta = min(beta, max_val)
+            if max_val <= alpha:
+                return max_val
+    return max_val
 
 
 def launch_alpha_beta(grid: Grid, depth: int, alpha: float, beta: float, rules, ai_value, is_max=True) -> tuple | None:
-	"""
-	Launch the alpha beta pruning to find the best move to play.
-	:param grid: the game board.
-	:param depth: the depth to explore.
-	:param alpha: the alpha limit.
-	:param beta:  the beta limit.
-	:param rules: game rules.
-	:param ai_value: the value used to determine the AI.
-	:param is_max: True if the turn is for the IA otherwise False.
-	:return: the move (x, y) as tuple or None in case of error.
-	"""
-	if depth <= 0:
-		return None
-	next_gen = next_generation(grid, rules, ai_value)
-	max_val = float('-inf')
-	move_selected = next_gen[0].get_last_move()[-2:]
-	for node in next_gen:
-		_val = alpha_beta(node, depth - 1, alpha, beta, rules, ai_value, is_max)
-		if _val > max_val:
-			max_val = _val
-			move_selected = node.get_last_move()[-2:]
-	return move_selected
+    """
+    Launch the alpha beta pruning (fail-soft) to find the best move to play.
+    :param grid: the game board.
+    :param depth: the depth to explore.
+    :param alpha: the alpha limit.
+    :param beta:  the beta limit.
+    :param rules: game rules.
+    :param ai_value: the value used to determine the AI.
+    :param is_max: True if the turn is for the IA otherwise False.
+    :return: the move (x, y) as tuple or None in case of error.
+    """
+    if depth <= 0:
+        return None
+    next_gen = next_generation(grid, rules, ai_value)
+    max_val = float('-inf')
+    move_selected = next_gen[0].get_last_move()[-2:]
+    for node in next_gen:
+        _val = alpha_beta(node, depth - 1, alpha, beta, rules, ai_value, is_max)
+        if _val > max_val:
+            max_val = _val
+            move_selected = node.get_last_move()[-2:]
+    return move_selected
