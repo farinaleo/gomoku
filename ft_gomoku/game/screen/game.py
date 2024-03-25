4bc52bedcb0b37c4680ca428f460a7b9a8b92350
nskiba <nskiba@student.42angouleme.fr>
2024-03-25 01:30:34 +0100

    :recycle: update button keydown

--- ft_gomoku/game/screen/game.py
+++ ft_gomoku/game/screen/game.py
@@ -6,11 +6,10 @@ import time
 
 from ft_gomoku.AI.AI import run_ai
 from ft_gomoku import Engine, set_titlescreen, stop_sound
-from ft_gomoku.game.screen.components import draw_board, place_rocks, redraw_board
+from ft_gomoku.game.screen.components import draw_board, place_rocks, redraw_board, show_capture, show_timer
 from ft_gomoku.data_structure.GameStruct import GameStruct
 from ft_gomoku.game.screen.animations import anim_win
 from ft_gomoku import RuleStatus, five_to_win, double_three_forbidden, capture, ten_capture_to_win
-
 from ft_gomoku.data_structure.DebuggerStruct import DebuggerStruct
 
 from ft_gomoku.game.debug.debug import debug_screen
@@ -37,7 +36,7 @@ def handle_events(engine, events_list, rocks_coord, game_engine: GameStruct, deb
 				return 'info'
 			if event.type == pygame.KEYDOWN and event.key == pygame.K_F2:
 				return 'help'
-		if event.type == pygame.MOUSEBUTTONDOWN:
+		if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
 			result = check_rocks_pos(rocks_coord, event.pos, radius)
 			if result is not None:
 				return place_rocks(engine.screen, game_engine, result, debug, 35)
@@ -59,48 +58,6 @@ def check_rocks_pos(rocks_coord: dict, mouse_pos: tuple, radius=15) -> tuple | N
 		if dx <= radius and dy <= radius:
 			return _coords, _board_coords
 	return None
-
-
-def show_timer(engine: Engine, game_engine: GameStruct):
-	"""Show the timer
-	:param engine: the game engine
-	:param game_engine: the game engine
-	"""
-	elapsed_time = int(time.time() - game_engine.get_time())
-	player_1_time = round(game_engine.total_time_player_1[1], 2)
-	player_2_time = round(game_engine.total_time_player_2[1], 2)
-	player_1_avg = round(sum(game_engine.list_time_player_1) / len(game_engine.list_time_player_1), 2) if len(game_engine.list_time_player_1) > 0 else 0
-	player_2_avg = round(sum(game_engine.list_time_player_2) / len(game_engine.list_time_player_2), 2) if len(game_engine.list_time_player_2) > 0 else 0
-
-	# Create the text
-	font = engine.font
-	text_timer = font.render(f"Time : {elapsed_time} seconds", True, (255, 255, 255))
-	text_rect = text_timer.get_rect(topleft=(10, 10))
-
-	text_p1_timer = font.render(f"Player 1 reflection time : {player_1_time} seconds", True, (255, 255, 255))
-	text_p1_rect = text_p1_timer.get_rect(topleft=(10, 40))
-
-	text_p2_timer = font.render(f"Player 2 reflection time : {player_2_time} seconds", True, (255, 255, 255))
-	text_p2_rect = text_p2_timer.get_rect(topleft=(10, 70))
-
-	text_p1_avg = font.render(f"Player 1 average reflection time : {player_1_avg} seconds", True, (255, 255, 255))
-	text_p1_avg_rect = text_p1_avg.get_rect(topleft=(10, 100))
-
-	text_p2_avg = font.render(f"Player 2 average reflection time : {player_2_avg} seconds", True, (255, 255, 255))
-	text_p2_avg_rect = text_p2_avg.get_rect(topleft=(10, 130))
-
-	# Fill a rectangle with the background color to clean screen
-	pygame.draw.rect(engine.screen, (8, 26, 43), text_rect)
-	pygame.draw.rect(engine.screen, (8, 26, 43), text_p1_rect)
-	pygame.draw.rect(engine.screen, (8, 26, 43), text_p2_rect)
-	pygame.draw.rect(engine.screen, (8, 26, 43), text_p1_avg_rect)
-	pygame.draw.rect(engine.screen, (8, 26, 43), text_p2_avg_rect)
-
-	engine.screen.blit(text_timer, (10, 10))
-	engine.screen.blit(text_p1_timer, (10, 40))
-	engine.screen.blit(text_p2_timer, (10, 70))
-	engine.screen.blit(text_p1_avg, (10, 100))
-	engine.screen.blit(text_p2_avg, (10, 130))
 
 
 def show_capture(engine: Engine, game_engine: GameStruct):
