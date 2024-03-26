f062a2bd304bcba6647c95ef49a38fca7e6f8145
Nolan Skiba <nolanskiba@icloud.com>
2024-03-26 04:38:41 +0100

    Requirement (#27)
    * :sparkles: add requirements
    
    * :hammer: add Makefile
    
    * :recycle: change how i get cpuinfo

--- ft_gomoku/data_structure/DebuggerStruct.py
+++ ft_gomoku/data_structure/DebuggerStruct.py
@@ -1,6 +1,6 @@
 import time
 import psutil
-import cpuinfo
+import platform, subprocess, re
 from dataclasses import dataclass
 from ft_gomoku.engine import Engine
 from ft_gomoku.data_structure import GameStruct
@@ -35,10 +35,14 @@ class DebuggerStruct:
 		self.captured_stones = game_engine.grid.captured_stones
 
 		#System information
-		self.python_version = ""
-		self.cpu_info = ""
-		self.cpu_cores = 0
-		self.cpu_arch = ""
+		if platform.system() == "Windows":
+			self.cpu_info = platform.processor()
+		elif platform.system() == "Linux":
+			command = "cat /proc/cpuinfo"
+			all_info = subprocess.check_output(command, shell=True).decode().strip()
+			for line in all_info.split("\n"):
+				if "model name" in line:
+					self.cpu_info = re.sub(".*model name.*:", "", line, 1)
 		self.cpu_freq = ""
 		self.ram_total = 0
 		self.ram_used = 0
@@ -68,59 +72,12 @@ class DebuggerStruct:
 
 	def update_cpu_info(self):
 		"""Update the cpu information in the debugger struct"""
-		cpu_info = cpuinfo.get_cpu_info()
-		if 'python_version' in cpu_info:
-			self.python_version = cpu_info['python_version']
-		if 'brand_raw' in cpu_info:
-			self.cpu_info = cpu_info['brand_raw']
-		if 'count' in cpu_info:
-			self.cpu_cores = cpu_info['count']
-		if 'arch' in cpu_info:
-			self.cpu_arch = cpu_info['arch']
-		if 'hz_actual_friendly' in cpu_info:
-			self.cpu_freq = cpu_info['hz_actual_friendly']
+		self.cpu_freq = psutil.cpu_freq(True)[0][0]
 
 	def update_ram_info(self):
 		"""Update the ram information in the debugger struct"""
 		self.ram_total = round((psutil.virtual_memory().total / (1024 ** 3)), 2)
 		self.ram_used = round((psutil.virtual_memory().used / (1024 ** 3)), 2)
-
-	def print_terminal(self):
-		"""Print the debugger information in the terminal"""
-
-		# print header stylized debugger message
-		print("+" + "-" * 78 + "+")
-		print("|" + " " * 78 + "|")
-		print("|" + " " * 30 + "DEBUGGER" + " " * 40 + "|")
-		print("|" + " " * 78 + "|")
-		print("+" + "-" * 78 + "+")
-
-		# print system information
-		print(f"Python version: {self.python_version}")
-		print(f"CPU: {self.cpu_cores}x {self.cpu_info} @ {self.cpu_freq} {self.cpu_arch}")
-		print(f"RAM: {self.ram_used}/{self.ram_total}GB")
-
-		# print settings file
-		print(f"Max FPS: {self.max_fps}")
-		print(f"Music: {'ON' if self.music_option else 'OFF'}")
-		print(f"Fullscreen: {'ON' if self.fullscreen_option else 'OFF'}")
-		print(f"Window size: {self.window_size}")
-
-		# print engine information
-		print(f"Current screen: {self.current_screen}")
-		print(f"Running: {self.running}")
-		print(f"Window size: {self.window_size}")
-
-		# print game information
-		print(f"Board grid: {self.board_grid}")
-		print(f"Player 1: {self.player_1}")
-		print(f"Player 2: {self.player_2}")
-		print(f"Grid size: {self.grid_size} # Board size, add 1 for the grid size")
-		print(f"Player turn: {self.player_turn}")
-		print(f"Opponent turn: {self.opponent_turn}")
-		print(f"Total time player 1: {self.total_time_player_1} # (total_time, last_time, tmp_start_time)")
-		print(f"Total time player 2: {self.total_time_player_2} # (total_time, last_time, tmp_start_time)")
-		print(f"Captured stones: {self.captured_stones}")
 
 	def get_all_game_info(self, reload: bool = False):
 		"""Get all the game information
@@ -147,10 +104,7 @@ class DebuggerStruct:
 			"total_time_player_1": self.total_time_player_1,
 			"total_time_player_2": self.total_time_player_2,
 			"captured_stones": self.captured_stones,
-			"python_version": self.python_version,
 			"cpu_info": self.cpu_info,
-			"cpu_cores": self.cpu_cores,
-			"cpu_arch": self.cpu_arch,
 			"cpu_freq": self.cpu_freq,
 			"ram_total": self.ram_total,
 			"ram_used": self.ram_used
